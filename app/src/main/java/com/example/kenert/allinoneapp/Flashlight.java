0a4998b3603f6d020ff8ed1cbf1e21809c9169fb
kenert11 <tsoojkk@gmail.com>
2016-11-04 17:44:56 +0200

    Flashlight first code

--- app/src/main/java/com/example/kenert/allinoneapp/Flashlight.java
+++ app/src/main/java/com/example/kenert/allinoneapp/Flashlight.java
@@ -1,13 +1,35 @@
 package com.example.kenert.allinoneapp;
 
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Flashlight extends AppCompatActivity {
+    private CameraManager cm;
+    private ImageButton flashlightButton;
+    private boolean flashlightOnOrOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashlight);
+        flashlightButton =(ImageButton) findViewById(R.id.flashOnOffButton);
+        flashlightOnOrOff = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
     }
 }
